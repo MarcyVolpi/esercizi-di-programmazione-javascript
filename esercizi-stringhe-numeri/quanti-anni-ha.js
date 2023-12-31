a1bdefabbf161b92ae2c92e272f000dd0635579a
MarcyVolpi <volpimarcella@gmail.com>
2023-12-31 14:06:38 +0100

    Result

--- esercizi-stringhe-numeri/quanti-anni-ha.js
+++ esercizi-stringhe-numeri/quanti-anni-ha.js
@@ -11,3 +11,12 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var annocorrente = 2023;
+var annonascita = 1994;
+
+var eta = annocorrente-annonascita;
+var anni100 = 100-eta;
+
+console.log('età: '+eta);
+console.log('anni mancanti: '+anni100);
